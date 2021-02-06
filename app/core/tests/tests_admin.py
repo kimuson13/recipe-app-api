7500d71286b897b2120d68fd5c8aa9b408eb2ad6
kimuson <kimuson13@basket.com>
2021-02-06 10:22:00 +0900

    modify django admin to list custom user model

--- app/core/tests/tests_admin.py
+++ app/core/tests/tests_admin.py
@@ -15,6 +15,7 @@ class AdminSiteTests(TestCase):
             password='password123',
             name='Test user full name'
         )
+
     def test_users_listed(self):
         """Test that users are listed on user page"""
 
@@ -24,3 +25,12 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+
+
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
